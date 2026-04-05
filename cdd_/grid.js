82f26446b5fa042a686cf806db291fc51893f016
deocze <255413163+deocze@users.noreply.github.com>
2026-04-05 09:26:00 -0400

    Add drawing feature

---
+++ cdd_/grid.js
@@ -0,0 +1,12 @@
+let container = document.querySelector(".drawingGrid");
+
+for (let i = 0; i < 16; i++){
+    let containingDiv = document.createElement("div");
+    containingDiv.className = "bigDiv"
+    container.append(containingDiv);
+    for (let i = 0; i < 16; i++){
+    let newDiv = document.createElement("div");
+    newDiv.className = "lilDiv";
+    containingDiv.append(newDiv);
+    }
+}
