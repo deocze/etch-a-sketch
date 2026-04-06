cc4624e0834c815fe9db795afb67fc4f69ff6935
deocze <255413163+deocze@users.noreply.github.com>
2026-04-06 06:34:55 -0400

    Added button for grid size

--- cdd_/grid.js
+++ cdd_/grid.js
@@ -4,9 +4,13 @@ for (let i = 0; i < 16; i++){
     let containingDiv = document.createElement("div");
     containingDiv.className = "bigDiv"
     container.append(containingDiv);
+    
     for (let i = 0; i < 16; i++){
     let newDiv = document.createElement("div");
     newDiv.className = "lilDiv";
     containingDiv.append(newDiv);
+
+    newDiv.addEventListener("mouseover",() => newDiv.style.backgroundColor = "black")
     }
+
 }
