4d8dd668ae606d510b7433b94540896f7a70d529
deocze <255413163+deocze@users.noreply.github.com>
2026-04-06 07:12:12 -0400

    Fix grid size button(Complete Project)

--- cdd_/grid.js
+++ cdd_/grid.js
@@ -1,27 +1,34 @@
 let container = document.querySelector(".drawingGrid");
 let gridButton = document.querySelector("button.gridSize");
 
-let gridValue = 0;
-gridButton.addEventListener(
-  "click",
-  () =>
-    (gridValue = prompt("How many squares do you want in the grid (1-100)")),
-);
+let gridValue = 16;
 
-for (let i = 0; i < 16; i++) {
-  let containingDiv = document.createElement("div");
-  containingDiv.className = "bigDiv";
-  container.append(containingDiv);
+generateGraph(gridValue);
 
-  for (let i = 0; i < 16; i++) {
-    let newDiv = document.createElement("div");
-    newDiv.className = "lilDiv";
-    containingDiv.append(newDiv);
-    let rgb = `${random()}`;
-    newDiv.addEventListener(
-      "mouseover",
-      () => (newDiv.style.backgroundColor = `rgb(${random()}, ${random()}, ${random()})`),
-    );
+gridButton.addEventListener("click", () => {
+  gridValue = prompt(
+    "How many squares by how much squares do you want in the grid (1-100)",
+  );
+  generateGraph(gridValue);
+});
+
+function generateGraph(gridValue) {
+  container.replaceChildren();
+  for (let i = 0; i < gridValue; i++) {
+    let containingDiv = document.createElement("div");
+    containingDiv.className = "bigDiv";
+    container.append(containingDiv);
+
+    for (let i = 0; i < gridValue; i++) {
+      let newDiv = document.createElement("div");
+      newDiv.className = "lilDiv";
+      containingDiv.append(newDiv);
+      newDiv.addEventListener(
+        "mouseover",
+        () =>
+          (newDiv.style.backgroundColor = `rgb(${random()}, ${random()}, ${random()})`),
+      );
+    }
   }
 }
 
