bae6af5264d2b3aeee6053affa2fd15932c07678
deocze <255413163+deocze@users.noreply.github.com>
2026-04-06 06:58:02 -0400

    Added RGB

--- cdd_/grid.js
+++ cdd_/grid.js
@@ -1,16 +1,31 @@
 let container = document.querySelector(".drawingGrid");
+let gridButton = document.querySelector("button.gridSize");
 
-for (let i = 0; i < 16; i++){
-    let containingDiv = document.createElement("div");
-    containingDiv.className = "bigDiv"
-    container.append(containingDiv);
-    
-    for (let i = 0; i < 16; i++){
+let gridValue = 0;
+gridButton.addEventListener(
+  "click",
+  () =>
+    (gridValue = prompt("How many squares do you want in the grid (1-100)")),
+);
+
+for (let i = 0; i < 16; i++) {
+  let containingDiv = document.createElement("div");
+  containingDiv.className = "bigDiv";
+  container.append(containingDiv);
+
+  for (let i = 0; i < 16; i++) {
     let newDiv = document.createElement("div");
     newDiv.className = "lilDiv";
     containingDiv.append(newDiv);
+    let rgb = `${random()}`;
+    newDiv.addEventListener(
+      "mouseover",
+      () => (newDiv.style.backgroundColor = `rgb(${random()}, ${random()}, ${random()})`),
+    );
+  }
+}
 
-    newDiv.addEventListener("mouseover",() => newDiv.style.backgroundColor = "black")
-    }
-
+function random() {
+  let num = Math.floor(Math.random() * 255);
+  return num;
 }
